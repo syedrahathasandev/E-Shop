0a5c92c16e5f295ec7e6bca4f40a2a12e9e6a6b1
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-07-28 20:20:22 +0600

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
+// import React from 'react'
 
-const HomePage = () => {
-  return (
-    <div>
-      Homepage
-    </div>
-  )
-}
-
-export default HomePage
+// const HomePage = () => {
+//   return (
+//     <div>
+//       Homepage
+//     </div>
+//   )
+// }
+// export default HomePage
