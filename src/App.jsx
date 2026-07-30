02d58f8505827311fbaba9e565120dfdc61a9b41
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-07-30 20:04:35 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-// import { BrowserRouter, Routes, Route } from "react-router";
-// import HomePage from "./pages/HomePage";
-// import ProductListPage from "./pages/ProductListPage";
-// function App() {
-//   return (
-// <Routes>
-//       <Route path="/" element={<HomePage />} />
-//       <Route path="/ product-list" element={< ProductListPage />} />
-//       <Route path="/" element={<HomePage />} />
-//       <Route path="/" element={<HomePage />} />
-//       <Route path="/" element={<HomePage />} />
-//       <Route path="/" element={<HomePage />} />
-//     </Routes>
-//   )
-// }
+import { BrowserRouter, Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import ProductListPage from "./pages/ProductListPage";
+function App() {
+  return (
+<Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/ product-list" element={< ProductListPage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  )
+}
 
-// export default App
+export default App
