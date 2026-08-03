1beacd94a6b272dc62401c264073feb89145ba8e
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-08-03 16:18:07 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProductListPage from "./pages/ProductListPage";
+import ProductListPage from "./pages/ProductDetailsPage";
+import ProductListPage from "./pages/ProductDetailsPage";
 function App() {
   return (
-<Routes>
+    <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/ product-list" element={< ProductListPage />} />
-      <Route path="/" element={<HomePage />} />
-      <Route path="/" element={<HomePage />} />
+      <Route path="/ product-details" element={<ProductDetailsPage />} />
+      <Route path="/ blog" element={<BlogPage />} />
       <Route path="/" element={<HomePage />} />
       <Route path="/" element={<HomePage />} />
     </Routes>
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProductListPage = () => {
   return (
     <div>
-      
+
     </div>
   )
 }
