8fb1ccb730652d91deb6ad00e8deb8bc8baa7f8d
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-08-27 11:15:44 +0600

    update

--- src/Components/Commonlayouts/CommonLayout.jsx
+++ src/Components/Commonlayouts/CommonLayout.jsx
@@ -1,17 +1,17 @@
-// import React from 'react'
-// import { Outlet } from 'react-router-dom'
-// import Container from './Container'
-// import Header from '../Header/Header'
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Container from './Container'
+import Header from '../Header/Header'
 
-// const CommonLayout = () => {
-//     return (
-//         <>
-//             <Header />
-//             <Outlet />
-//             <div>Footer</div>
-//         </>
+const CommonLayout = () => {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <div>Footer</div>
+        </>
 
-//     )
-// }
+    )
+}
 
-// export default CommonLayout
+export default CommonLayout
